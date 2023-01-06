aa2ac13703734e68368d0123a3df14f00a3f7f72
Andrii Kyrychenko <akyrych84@gmail.com>
2023-01-06 18:10:00 -0400

    Initial Commit

---
+++ Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Andrii Kyrychenko on 06/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
